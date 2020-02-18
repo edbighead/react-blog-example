cdfec69b6a6fcd70dddad9dd01e0e1073a7bcf90
Eduard Laur <Eduard.Laur@endava.com>
2020-02-18 13:51:38 +0200

    Change background color

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ export class Header extends PureComponent {
           ) : null
         }
         showMenuIconButton={isPost}
-        style={{ textAlign: 'center', backgroundColor: '#db1499' }}
+        style={{ textAlign: 'center', backgroundColor: '#888444' }}
       />
     );
   }
